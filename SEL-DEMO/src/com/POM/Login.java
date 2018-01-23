1e2c294ef0465383a95901ecdd5dad5e1dbe9319
Narender Balla <nare4252@gmail.com>
2018-01-23 15:32:37 +0530

    POM

---
+++ SEL-DEMO/src/com/POM/Login.java
@@ -0,0 +1,19 @@
+package com.POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.annotations.Configuration;
+
+public class Login extends Configuration{
+	
+	By userID = By.xpath("");
+	By userPass = By.xpath("");
+	By titleText = By.xpath("");
+	By submitButton = By.xpath("");
+	
+	public Login(WebDriver driver){
+		this.driver = driver;
+	}
+	
+}
